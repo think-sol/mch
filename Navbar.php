3fa52e1340bb71f823121e24dc58b081f0e0676f
Syed Mutahir <mutahir@thinksolutionz.org>
2020-03-26 21:55:47 +0500

    initial commit

---
+++ Navbar.php
@@ -0,0 +1,33 @@
+
+    <div class="sticky-top">
+        <nav class="navbar navbar-expand-lg" style="padding:20px;background-color:rgba(255, 255, 255, 0.7);">
+          <a href="./Index.php" class="navbar-brand text-dark">
+            <img src="./Images/logo.png" style="width:200px" />
+          </a>
+          <button class="navbar-toggler toggle-btn" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">
+              <i class="fa fa-bars" style="font-size:30px"></i>
+            </span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link text-dark link" href="./Index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-dark link" href="./Expertise.php">Expertise</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-dark link" href="./Client.php">Client</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-dark link" href="./About.php">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-dark link" href="./Contact.php">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+    </div>
+   
