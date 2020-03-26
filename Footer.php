cc60ff95e002ddd4342ea547f54f2fcf1eb062e5
Syed Mutahir <mutahir@thinksolutionz.org>
2020-03-26 22:38:35 +0500

    initial commit

---
+++ Footer.php
@@ -0,0 +1,57 @@
+<div class="footer-container">
+    <div class="container">
+        <div class="row footer-image">
+            <img data-aos="fade-up" data-aos-duration="1200" src="./Images/logo2.png" style="width:250px" />
+        </div>
+        <div class="row footer-links">
+            <div class="col-xl-6 col-sm-6" style="display:flex;justify-content:center">
+                <div data-aos="zoom-in" data-aos-duration="1200">
+                    <div style="display:flex;flex-direction:column;align-items:center">
+                        <div class="footer-menu">
+                            <i class="fa fa-bars" style="font-size:30px;color:white"></i>
+                        </div>
+                        <h5 style="color:white;font-weight:bold;align-self:center;margin-top:20px">Quick Links</h5>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <ul style="list-style:none;display:flex;justify-content:space-between;padding:10px">
+                                <li><a href="#" style="text-decoration:none;color:white;margin:10px"> Expertise </a></li>
+                                <li><a href="#" style="text-decoration:none;color:white;margin:10px"> Client </a></li>
+                                <li><a href="#" style="text-decoration:none;color:white;margin:10px"> About </a></li>
+                                <li><a href="#" style="text-decoration:none;color:white"> Contant</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-6 col-sm-6" style="display:flex;justify-content:center">
+                <div data-aos="zoom-in" data-aos-duration="1200">
+                    <div style="display:flex;flex-direction:column;align-items:center" >
+                        <div class="footer-menu">
+                            <i class="fa fa-bars" style="font-size:30px;color:white"></i>
+                        </div>
+                        <h5 style="color:white;font-weight:bold;align-self:center;margin-top:20px">Social Links</h5>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <ul style="list-style:none;display:flex;justify-content:space-between;padding:10px">
+                                <li><a href="#" style="text-decoration:none;color:white;margin:20px"> <i class="fa fa-facebook-square" style="color:white;font-size:30px"></i> </a></li>
+                                <li><a href="#" style="text-decoration:none;color:white;margin:20px"> <i class="fa fa-twitter-square" style="color:white;font-size:30px"></i> </a></li>
+                                <li><a href="#" style="text-decoration:none;color:white;margin:20px"> <i class="fa fa-instagram" style="color:white;font-size:30px"></i> </a></li>
+                                <li><a href="#" style="text-decoration:none;color:white"> <i class="fa fa-linkedin" style="color:white;font-size:30px"></i> </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row copy-right">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 copyright-box1">
+                <p  style="color:white">@Copy-Right <b>MCH Strategies</b></p>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 copyright-box2">
+                <p style="color:white"><b>Contact: </b> 033333333 </p>
+            </div>
+        </div>
+    </div>
+</div>
